c33363a44b6a2419415e4f580c70d3b936fdebbd
Anders Brams <anders@brams.dk>
2024-09-28 17:48:33 +0200

    Bump @0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as fp:
 
 setuptools.setup(
     name="fastapi-decorators",
-    version="0.0.2",
+    version="0.0.3",
     author="Anders Brams",
     author_email="anders@brams.dk",
     description="Decorate FastAPI endpoints with custom decorators.",
