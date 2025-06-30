6ee3cac2cc55c3deabb61b5f12d5c6fbc39b86d1
Pegler, Sam <Sam.Pegler@britinsurance.com>
2025-06-30 18:36:34 +0100

    Bug: Add failing test.

---
+++ tests/forwardrefs/dummy_decorator.py
@@ -0,0 +1,26 @@
+from functools import wraps
+from typing import Any
+
+from fastapi import Depends
+
+from fastapi_decorators import depends
+from fastapi_decorators.types import F, Decorator
+
+
+def get_nothing() -> None:
+    return None
+
+
+def empty_decorator(func: F) -> F:
+    @depends(test=Depends(get_nothing))
+    @wraps(func)
+    def wrapper(
+        *args: Any,
+        test: None,
+        **kwargs: Any,
+    ) -> Any:
+        # Don't do anything
+        result = func(*args, **kwargs)
+        return result
+
+    return wrapper
