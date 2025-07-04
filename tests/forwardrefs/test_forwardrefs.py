a1e60d496595899d30c6c5a35ba9f854fcae8296
Pegler, Sam <Sam.Pegler@britinsurance.com>
2025-07-04 11:13:30 +0100

    Bug: Add extra test that covers forward refs plus annotations.

--- tests/forwardrefs/test_forwardrefs.py
+++ tests/forwardrefs/test_forwardrefs.py
@@ -1,10 +1,10 @@
 """This app JUST tests forward references."""
 
 from __future__ import annotations
-
+from typing import Annotated
 
 import pytest
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Header
 from fastapi.testclient import TestClient
 
 from tests.forwardrefs.dummy_decorator import empty_decorator
@@ -32,5 +32,23 @@ def test_forward_references_are_copied_from_call_site_scope(
     test_client: TestClient,
 ) -> None:
     response = test_client.get("/test")
+    assert response.status_code == 200
+    assert response.json() == "OK"
+
+
+def test_forward_references_plus_annotation(
+    test_client: TestClient,
+) -> None:
+    @test_client.app.get("/test-extra")
+    @empty_decorator
+    def endpoint_with_request_dependency(
+        request: Request,
+        requestor_id: Annotated[str, Header(alias="requestor_id")],
+    ) -> str:
+        """Test a builtin."""
+        return "OK"
+
+    headers = {"requestor_id": "potato@me"}
+    response = test_client.get("/test-extra", headers=headers)
     assert response.status_code == 200
     assert response.json() == "OK"
